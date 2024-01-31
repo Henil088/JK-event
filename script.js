026fb51f59fd2f455805ee016ffd92121b421156
Henil <136146922+Henil088@users.noreply.github.com>
2024-01-31 19:20:31 +0530

    Files
    This is JK event Files

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+
+menu.onclik = () =>{
+
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.scroll = () =>{
+
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+
