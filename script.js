88cb99e919efbef9368a947cf4f2233b87f15b18
Henil <136146922+Henil088@users.noreply.github.com>
2024-04-19 23:37:02 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
-
-
 let menu = document.querySelector('#menu-bars');
-let navbar = document.querySelector('.navbar');
+let navbar = document.querySelector('nav');
 
 
-menu.onclik = () =>{
-
-    menu.classList.toggle('fa fa-times');
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    
+    menu.classList.toggle('fa-bars');
     navbar.classList.toggle('active');
+    navbar.classList.toggle('navbar');
+    
 }
 
-window.scroll = () =>{
 
+window.onscroll = () =>{
     menu.classList.remove('fa fa-times');
-    navbar.classList.remove('active');
+    
+    menu.classList.toggle('fa fa-bars');
+    // navbar.classList.remove('active');
 }
 
-
+var swiper = new Swiper(".home-slider", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    loop: true,
+    autoplay:{
+        delay: 3000,
+        desableOnInteraction: false,
+    }
+  });
